c250dc11b5e8622ada50281fdb81ce1af487150d
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-03-29 14:07:15 +0200

    Update books_DB.sql

--- books_DB.sql
+++ books_DB.sql
@@ -3,25 +3,31 @@ CREATE TABLE books (
 	title VARCHAR (120),
 	author VARCHAR (120),
 	original_language VARCHAR (120),
-	sales INT
+	sales INT,
+	classify_under VARCHAR (120)
 
 	
 );
-INSERT INTO books (id, title, author, original_language, sales) VALUES (1, 'The Hobbit', 'J. R. R. Tolkien', 'English', 100);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (2, 'Watership Down', 'Richard Adams', 'English', 50);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (3, 'Harry Potter and the Philosopher''s Stone', 'J. K. Rowling', 'English', 120);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (4, 'The Plague', 'Albert Camus', 'French', 12);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (5, 'The Divine Comedy', 'Dante Alighieri', 'Italian', 12);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (6, 'War And Peace', 'Leo Tolstoy', 'Russian', 36);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (7, 'Nineteen Eighty-Four', 'George Orwell', 'English', 30);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (8, 'Andromeda Nebula', 'Ivan Yefremov', 'Russian', 20);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (9, 'The Little Prince', 'Antoine de Saint-Exupéry', 'French', 200);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (10, 'The Stranger', 'Albert Camus', 'French', 10);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (11, 'The Adventures of Pinocchio', 'Carlo Collodi', 'Italian', 35);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (12, 'The Name of the Rose', 'Umberto Eco', 'Italian', 50);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (13, 'One Hundred Years of Solitude', 'Gabriel García Márquez', 'Spanish', 50);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (14, 'Don Quixote', 'Miguel de Cervantes', 'Spanish', 500);
-INSERT INTO books (id, title, author, original_language, sales) VALUES (15, 'Lolita', 'Vladimir Nabokov', 'English', 50);
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (1, 'The Hobbit', 'J. R. R. Tolkien', 'English', 100, 'Fantasy');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (2, 'Watership Down', 'Richard Adams', 'English', 50, 'Fantasy');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (3, 'Harry Potter and the Philosopher''s Stone', 'J. K. Rowling', 'English', 120, 'Fantasy');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (4, 'The Plague', 'Albert Camus', 'French', 12, 'Classics');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (5, 'The Divine Comedy', 'Dante Alighieri', 'Italian', 12, 'Poetry');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (6, 'War And Peace', 'Leo Tolstoy', 'Russian', 36, 'Classics');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (7, 'Nineteen Eighty-Four', 'George Orwell', 'English', 30, 'Classics');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (8, 'Andromeda Nebula', 'Ivan Yefremov', 'Russian', 20, 'Science fiction');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (9, 'The Little Prince', 'Antoine de Saint-Exupéry', 'French', 200, 'Kids');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (10, 'The Stranger', 'Albert Camus', 'French', 10, 'Classics');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (11, 'The Adventures of Pinocchio', 'Carlo Collodi', 'Italian', 35, 'Kids');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (12, 'The Name of the Rose', 'Umberto Eco', 'Italian', 50, 'Classics');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (13, 'One Hundred Years of Solitude', 'Gabriel García Márquez', 'Spanish', 50, 'Classics');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (14, 'Don Quixote', 'Miguel de Cervantes', 'Spanish', 500, 'Classics');
+INSERT INTO books (id, title, author, original_language, sales, classify_under) VALUES (15, 'Lolita', 'Vladimir Nabokov', 'English', 50, 'Classics');
+
+
+
+
+
 
 
 
